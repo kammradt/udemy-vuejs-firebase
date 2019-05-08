825e12be29721b1e017bc5384f1db2a7246b90cc
kammradt <vinicius.kammradt1@gmail.com>
2019-05-08 19:48:16 -0300

    :sparkles: - data() and methods()

---
+++ basic-review/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Hi there!</h1>
+    <p>{{ text }}</p>
+    <p>{{ sayHello() }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      text: 'Variable!',
+      name: 'John'
+    }
+  },
+  methods:{
+    sayHello() {
+      return `Hello ${this.name}!`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

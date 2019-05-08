b0b33f1470417ed8cf44e77ce1bd03d2e20b790a
kammradt <vinicius.kammradt1@gmail.com>
2019-05-08 20:00:39 -0300

    :sparkles: - Using 'Arrow functions'

--- basic-review-es6/arrowFunctions.js
+++ basic-review-es6/arrowFunctions.js
@@ -1,5 +1,18 @@
-const greet = function (name) {
+const greetName = function (name) {
     console.log(`Hi ${name}!`)
 }
 
-greet('Vini')
+const greetArrow = name => {
+    console.log(`Hi ${name}!`)
+}
+
+const greetFriend = () => {
+    console.log('Hello friend!')
+}
+
+const greet = () => console.log('Hi')
+
+greetName('Vini')
+greetArrow('Kamm')
+greetFriend()
+greet()

b3e799b30ce1f84fdb3ce1be8f1c66777f199feb
kammradt <vinicius.kammradt1@gmail.com>
2019-06-26 21:12:58 -0300

    :sparkles: Learning how to use Vue Router
    Learning how I can route through different component s, use dynamic links and use/get url params for rotues

---
+++ learning-routing/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li> <router-link :to="{name: 'Home'}">Home</router-link> </li>
+      <li> <router-link :to="{name: 'About'}">About</router-link> </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>

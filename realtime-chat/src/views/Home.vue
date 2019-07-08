bd646914a8a68a918d50d433a01d7d41aa3a8611
kammradt <vinicius.kammradt1@gmail.com>
2019-07-08 01:09:47 -0300

    :lipstick: Creating 'Welcome' template

--- realtime-chat/src/views/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <v-btn color="success">text</v-btn>
-  </div>
-</template>
---
+++ realtime-chat/src/views/Welcome.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container align-content-center>
+    <v-card class="ma-3">
+      <v-flex
+        v-text="'Welcome to vue-chat 💬'"
+        teal--text
+        font-weight-light
+        display-2
+        text-xs-center
+        pt-4
+      />
+      <v-form ref="form" @submit.prevent="enterChat" class="ma-3">
+        <v-text-field
+          label="Name"
+          placeholder="Enter your name"
+          v-model="name"
+          :rules="nameRules"
+          :counter="20"
+          required
+        />
+      </v-form>
+      <v-card-actions>
+        <v-btn
+          @click="enterChat"
+          v-text="'Enter'"
+          color="primary"
+          class="headline font-weight-light"
+          block
+        />
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: null,
+    nameRules: [
+      v => (!!v) || 'Name is required',
+      v => (v && v.length <= 20) || 'Name must be less than 20 characters',
+      v => (v && v.length >= 5) || 'Name must be at least 5 characters'
+    ]
+  }),
+
+  methods: {
+    enterChat () {
+      if (this.$refs.form.validate()) {
+        console.log(this.name)
+        this.clear()
+      }
+    },
+    clear () {
+      this.$refs.form.reset()
+    }
+  }
+}
+</script>
